e736f99df0d7186903ec972b8b320674eec3463b
betrye <andreyrussprint@gmail.com>
2020-11-24 20:14:44 +0700

    Setup Koin

--- app/src/main/java/com/betrye/addressbook/App.kt
+++ app/src/main/java/com/betrye/addressbook/App.kt
@@ -1,5 +1,34 @@
 package com.betrye.addressbook
 
 import android.app.Application
+import com.betrye.addressbook.di.dataModule
+import com.betrye.addressbook.di.domainModule
+import com.betrye.addressbook.di.fragmentModule
+import com.betrye.addressbook.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.fragment.koin.fragmentFactory
+import org.koin.core.KoinExperimentalAPI
+import org.koin.core.context.startKoin
 
-class App : Application()
+@KoinExperimentalAPI
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        setupKoin()
+    }
+
+    private fun setupKoin() {
+        startKoin {
+            androidContext(this@App)
+            fragmentFactory()
+            modules(listOf(
+                domainModule,
+                dataModule,
+                viewModelModule,
+                fragmentModule,
+            ))
+        }
+    }
+}

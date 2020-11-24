e45450a96a69f6748f1d46db5c98b1ec1f40e192
betrye <andreyrussprint@gmail.com>
2020-11-24 18:54:16 +0700

    Add local data entity

---
+++ app/src/main/java/com/betrye/addressbook/data/entity/ContactLocal.kt
@@ -0,0 +1,7 @@
+package com.betrye.addressbook.data.entity
+
+class ContactLocal(
+    val id: Long,
+    val name: String,
+    val avatarUri: String?
+)
---
+++ app/src/main/java/com/betrye/addressbook/data/entity/PhoneLocal.kt
@@ -0,0 +1,7 @@
+package com.betrye.addressbook.data.entity
+
+class PhoneLocal(
+    val contactId: Long,
+    val number: String,
+    val typeCode: Int
+)
